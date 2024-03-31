b2947a233950136ec8e49d8731f310f39d599c7d
Allan Burleson <allanburleson@users.noreply.github.com>
2024-03-30 23:46:58 -0500

    Move BLE code to bluetooth.hpp (not yet functional)

--- include/bluetooth.hpp
+++ include/bluetooth.hpp
@@ -7,7 +7,18 @@
 #include <BLEUtils.h>
 #include <sstream>
 
-bool deviceConnected = false;
+#define RSSI_CONNECT -80
+#define RSSI_DISCONNECT -90
+#define SCAN_DURATION 1
+#define SCAN_WINDOW 100
+#define SCAN_INTERVAL 500
+
+// globals BAD
+bool clientConnected = false;
+bool serverConnected = false;
+boolean doServerConnect = false;
+BLEAdvertisedDevice *peripheral_device;
+
 typedef struct __attribute__((__packed__)) BluetoothTransmissionData
 {
     float temp_data;
@@ -60,9 +71,17 @@ class CharacteristicCallbacks : public BLECharacteristicCallbacks
         pCharacteristic->setValue(str_data.str());
     }
 
-    void onNotify(BLECharacteristic *pCharacteristic)
+    void onNotify(BLECharacteristic *pCharacteristic, uint8_t *pData, size_t length,
+                           bool isNotify)
     {
-        printf("NOTIFED\n");
+        Serial.print("Notify data from peripheral: ");
+        // print entire recieved packet by byte
+        for (int i = 0; i < length; i++)
+        {
+            Serial.print(*(pData + i), HEX);
+        }
+        Serial.print(". Length: ");
+        Serial.println(length);
     }
 
     void onStatus(BLECharacteristic *pCharacteristic, Status s, uint32_t code)
@@ -86,14 +105,14 @@ class ServerCallbacks : public BLEServerCallbacks
   public:
     void onConnect(BLEServer *pServer)
     {
-        deviceConnected = true;
+        clientConnected = true;
         Serial.printf("Connected....\n");
         BLEDevice::stopAdvertising();
     }
 
     void onDisconnect(BLEServer *pServer)
     {
-        deviceConnected = false;
+        clientConnected = false;
         Serial.printf("Disconnected....\n");
         BLEDevice::startAdvertising();
     }
@@ -117,7 +136,7 @@ class AdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
         {
             Serial.println(advertisedDevice.haveServiceUUID());
             peripheral_device = new BLEAdvertisedDevice(advertisedDevice);
-            doConnect = true;
+            doServerConnect = true;
             Serial.println("Peripheral found.");
         }
         else
@@ -134,7 +153,7 @@ class ClientCallback : public BLEClientCallbacks
 
     void onDisconnect(BLEClient *pclient)
     {
-        connected = false;
+        serverConnected = false;
         digitalWrite(LED_BUILTIN, LOW);
         Serial.println("onDisconnect...");
     }
@@ -148,6 +167,8 @@ template <typename _UUID_Generator_Type> class Bluetooth
     BLECharacteristic *pCharacteristic;
     BLEAdvertising *pAdvertising;
     _UUID_Generator_Type _uuid_gen_struct;
+    BLEScan *pBLEScan;
+    std::vector<BLEClient *> clients; // one BLEClient object per connected server
 
   public:
     CharacteristicCallbacks *callback_class;
@@ -160,7 +181,7 @@ template <typename _UUID_Generator_Type> class Bluetooth
     {
         callback_class = new CharacteristicCallbacks();
 
-        BLEDevice::init("TEMP-0");
+        BLEDevice::init("A0");
         pServer = BLEDevice::createServer();
         pService = pServer->createService(_uuid_gen_struct.get_service_uuid());
         pCharacteristic = pService->createCharacteristic(
@@ -176,11 +197,19 @@ template <typename _UUID_Generator_Type> class Bluetooth
         pAdvertising->setScanResponse(false);
         pAdvertising->setMinPreferred(0x0);
         pAdvertising->start();
+        // will add semaphore so advertising and scanning can't be simultaneous
+        BLEDevice::init("A0");
+        pBLEScan = BLEDevice::getScan();
+        pBLEScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
+        pBLEScan->setInterval(SCAN_INTERVAL);
+        pBLEScan->setWindow(SCAN_WINDOW);
+        pBLEScan->setActiveScan(true);
+
     }
 
     void sendData()
     {
-        if (deviceConnected)
+        if (clientConnected)
         {
             BluetoothTransmissionDataConverter_t converter;
             converter.message = callback_class->getData();
@@ -189,7 +218,75 @@ template <typename _UUID_Generator_Type> class Bluetooth
         }
     }
 
-	void connect_to_
+    bool tryConnectToServer()
+    {
+        if (doServerConnect)
+        {
+            doServerConnect = false;
+            Serial.print("Forming a connection to ");
+            Serial.println(peripheral_device->getAddress().toString().c_str());
+
+            BLEClient *pClient = BLEDevice::createClient();
+            clients.push_back(pClient);
+
+            pClient->setClientCallbacks(new ClientCallback());
+
+            // Connect to the remote BLE Server.
+            pClient->connect(peripheral_device);
+            Serial.println(" - Connected to server");
+            pClient->setMTU(40); // arbitrary MTU. The max is supposedly 517
+
+            // prep primary service object
+            BLERemoteService *pRemoteService = pClient->getService(serviceUUID);
+            if (pRemoteService == nullptr)
+            {
+                Serial.print("Failed to find our service UUID: ");
+                Serial.println(serviceUUID.toString().c_str());
+                pClient->disconnect();
+                return false;
+            }
+
+            // prep characteristic object (Just one for now. Later: add multiple for sharing other info (battery status etc.))
+            BLERemoteCharacteristic *pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+            if (pRemoteCharacteristic == nullptr)
+            {
+                Serial.print("Failed to find our characteristic UUID: ");
+                Serial.println(charUUID.toString().c_str());
+                pClient->disconnect();
+                return false;
+            }
+
+            // prep to receive notifications from peripheral
+            pRemoteCharacteristic->registerForNotify(notifyCallback);
+
+            serverConnected = true;
+            digitalWrite(LED_BUILTIN, HIGH);
+            return true;
+        }
+
+        return false;
+    }
+
+    void scan()
+    {
+        pBLEScan->start(SCAN_DURATION, false);
+    }
+    
+    void removeOldServers()
+    {
+        BLEClient *client;
+        for (int i = 0; i < clients.size(); i++)
+        {
+            client = clients[i];
+            if (client->getRssi() < RSSI_DISCONNECT)
+            {
+                Serial.println("RSSI fell below threshold. Disconnecting from server.");
+                client->disconnect();
+                clients.erase(clients.begin() + i);
+                i--;
+            }
+        }
+    }
 };
 
 #endif /* A3A894A8_7557_423C_AF30_DF2AD1257EE6 */
--- src/main.cpp
+++ src/main.cpp
@@ -5,100 +5,9 @@
 #include "auxiliary.h"
 #include "bluetooth.hpp"
 
-#define SCAN_DURATION 1
-#define SCAN_WINDOW 100
-#define SCAN_INTERVAL 500
-
-// RSSI thresholds
-#define RSSI_CONNECT -80
-#define RSSI_DISCONNECT -90
 
 uuids UUID_generator;
-
-static boolean doConnect = false;
-static boolean connected = false;
-static BLEAdvertisedDevice *peripheral_device;
-BLEScan *pBLEScan;
-std::vector<BLEClient *> clients; // one BLEClient object per connected server
-
-hw_timer_t *timer_5sec = NULL;
-
-void notifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pData, size_t length,
-                           bool isNotify)
-{
-    Serial.print("Notify data from peripheral: ");
-    // print entire recieved packet by byte
-    for (int i = 0; i < length; i++)
-    {
-        Serial.print(*(pData + i), HEX);
-    }
-    Serial.print(". Length: ");
-    Serial.println(length);
-}
-
-bool connectToServer()
-{
-    Serial.print("Forming a connection to ");
-    Serial.println(peripheral_device->getAddress().toString().c_str());
-
-    BLEClient *pClient = BLEDevice::createClient();
-    clients.push_back(pClient);
-
-    pClient->setClientCallbacks(new ClientCallback());
-
-    // Connect to the remote BLE Server.
-    pClient->connect(peripheral_device);
-    Serial.println(" - Connected to server");
-    pClient->setMTU(40); // arbitrary MTU. The max is supposedly 517
-
-    // prep primary service object
-    BLERemoteService *pRemoteService = pClient->getService(serviceUUID);
-    if (pRemoteService == nullptr)
-    {
-        Serial.print("Failed to find our service UUID: ");
-        Serial.println(serviceUUID.toString().c_str());
-        pClient->disconnect();
-        return false;
-    }
-
-    // prep characteristic object (Just one for now. Later: add multiple for sharing other info (battery status etc.))
-    BLERemoteCharacteristic *pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
-    if (pRemoteCharacteristic == nullptr)
-    {
-        Serial.print("Failed to find our characteristic UUID: ");
-        Serial.println(charUUID.toString().c_str());
-        pClient->disconnect();
-        return false;
-    }
-
-    // prep to receive notifications from peripheral
-    pRemoteCharacteristic->registerForNotify(notifyCallback);
-
-    connected = true;
-    digitalWrite(LED_BUILTIN, HIGH);
-    return true;
-}
-
-void scan()
-{
-    pBLEScan->start(SCAN_DURATION, false);
-}
-
-void disconnect_bad_servers()
-{
-    BLEClient *client;
-    for (int i = 0; i < clients.size(); i++)
-    {
-        client = clients[i];
-        if (client->getRssi() < RSSI_DISCONNECT)
-        {
-            Serial.println("RSSI fell below threshold. Disconnecting from server.");
-            client->disconnect();
-            clients.erase(clients.begin() + i);
-            i--;
-        }
-    }
-}
+Bluetooth<uuids> bluetooth;
 
 void setup()
 {
@@ -107,34 +16,13 @@ void setup()
     UUID_generator.generate_hashes();
     Serial.printf("SERVICE UUID - %s\n", UUID_generator.get_service_uuid());
     Serial.printf("SERVICE UUID - %s\n", UUID_generator.get_characteristic_uuid());
-
-    BLEDevice::init("AGGREGATOR");
-    pBLEScan = BLEDevice::getScan();
-    pBLEScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
-    pBLEScan->setInterval(SCAN_INTERVAL);
-    pBLEScan->setWindow(SCAN_WINDOW);
-    pBLEScan->setActiveScan(true);
-
-    scan();
+    bluetooth = Bluetooth<uuids>(UUID_generator);
 }
 
 void loop()
 {
-    if (doConnect)
-    {
-        Serial.println("Trying to connect...");
-        if (connectToServer())
-        {
-            Serial.println("Connected!");
-        }
-        else
-        {
-            Serial.println("Connection failed.");
-        }
-        doConnect = false;
-    }
+    bluetooth.tryConnectToServer();
     // ALWAYS scan, because devices could become available at any time
-
-    scan();
-    disconnect_bad_servers(); // ultimately make this asynchronous with FreeRTOS task
+    bluetooth.scan();
+    bluetooth.removeOldServers();
 }
