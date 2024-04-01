91ad1405558befddf44a818fafa5e000fc798f29
Allan Burleson <allanburleson@users.noreply.github.com>
2024-04-01 17:37:13 -0500

    Hardcode UUIDs

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@ void setup()
 {
     Serial.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
-    UUID_generator.initialize_random_values();
-    UUID_generator.generate_hashes();
+    //UUID_generator.initialize_random_values();
+    //UUID_generator.generate_hashes();
     Serial.printf("SERVICE UUID - %s\n", UUID_generator.get_service_uuid());
     Serial.printf("CHARACTERISTIC UUID - %s\n", UUID_generator.get_characteristic_uuid());
     bluetooth = Bluetooth<uuids>(UUID_generator);
