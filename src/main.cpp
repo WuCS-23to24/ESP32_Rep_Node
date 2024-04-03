84d6f9bb3fe67f980b069885f9facc35316a345a
Allan Burleson <allanburleson@users.noreply.github.com>
2024-04-03 18:07:26 -0500

    Interrupt timer for scanning

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,29 @@
 
 #include "auxiliary.h"
 #include "bluetooth.hpp"
+#include "hardware_timer.hpp"
 
 
 uuids UUID_generator;
 Bluetooth<uuids> bluetooth;
+
+volatile SemaphoreHandle_t scan_semaphore;
+volatile int8_t SCAN_ISR = 0;
+portMUX_TYPE isr_mux = portMUX_INITIALIZER_UNLOCKED;
+
+void ARDUINO_ISR_ATTR set_semaphore()
+{
+    taskENTER_CRITICAL_ISR(&isr_mux);
+    if (SCAN_ISR == 6)
+    {
+        xSemaphoreGiveFromISR(scan_semaphore, NULL);
+    }
+    else
+    {
+        SCAN_ISR++;
+    }
+    taskEXIT_CRITICAL_ISR(&isr_mux);
+}
 
 void setup()
 {
@@ -18,6 +37,9 @@ void setup()
     Serial.printf("SERVICE UUID - %s\n", UUID_generator.get_service_uuid());
     Serial.printf("CHARACTERISTIC UUID - %s\n", UUID_generator.get_characteristic_uuid());
     bluetooth = Bluetooth<uuids>(UUID_generator);
+    
+    scan_semaphore = xSemaphoreCreateBinary();
+    setup_timer(set_semaphore, 250, 80);
 }
 
 void loop()
@@ -25,4 +47,9 @@ void loop()
     bluetooth.tryConnectToServer();
     bluetooth.scan();
     bluetooth.removeOldServers();
+
+    if (xSemaphoreTake(scan_semaphore, 0) == pdTRUE)
+    {
+        bluetooth.scan();
+    }
 }
